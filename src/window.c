ac8f6f1ba7b5c6277456d02ff1349091e71519b5
Pierre Pronchery <khorben@defora.org>
2015-09-12 00:37:20 -0400

    Update the copyright notice

--- src/window.c
+++ src/window.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 static char const _copyright[] =
-"Copyright © 2013 Pierre Pronchery <khorben@defora.org>";
+"Copyright © 2013-2015 Pierre Pronchery <khorben@defora.org>";
 /* This file is part of DeforaOS Desktop HexEditor */
 static char const _license[] =
 "This program is free software: you can redistribute it and/or modify\n"
